a2e3c0fd771b27f4ff0e5819cde32e2ffc14334d
andreynetrebin <andrei.sanich89@yandex.ru>
2025-01-21 21:46:28 +0300

    changed pagination for list movies

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,7 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
+from django.core.paginator import Paginator
 from django.shortcuts import get_object_or_404
 from .forms import MovieCreateForm
 from django.db.models import Count
@@ -161,48 +162,63 @@ def movie_detail(request, slug):
 @login_required
 def movie_list(request):
     movies = Movie.objects.all()
-    watched_movies = request.user.watched_set.all()  # Получаем все просмотренные фильмы
-    wishlist_movies = request.user.wishlist_set.all()  # Получаем все фильмы в вишлисте
+    paginator = Paginator(movies, 10)  # Показывать 10 фильмов на странице
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    # Получаем топ-10 режиссеров по количеству фильмов
+    # Получаем топ-10 режиссеров и сценаристов
     top_directors = Director.objects.annotate(num_movies=Count('movies_director')).order_by('-num_movies')[:10]
-    # Получаем топ-10 сценаристов по количеству фильмов
     top_writers = Writer.objects.annotate(num_movies=Count('movies_writer')).order_by('-num_movies')[:10]
 
-    paginator = Paginator(movies, 5)
-    page = request.GET.get('page')
-    movies_only = request.GET.get('movies_only')
-    try:
-        movies = paginator.page(page)
-    except PageNotAnInteger:
-    # Если страница не является целым числом,
-    # то доставить первую страницу
-        movies = paginator.page(1)
-    except EmptyPage:
-        if movies_only:
-        # Если AJAX-запрос и страница вне диапазона,
-        # то вернуть пустую страницу
-            return HttpResponse('')
-    # Если страница вне диапазона,
-    # то вернуть последнюю страницу результатов
-        movies = paginator.page(paginator.num_pages)
-    if movies_only:
-        return render(request,
-    'movies/movie/list_movies.html',
-    {'section': 'movies',
-    'movies': movies,
-      'watched_movies': watched_movies,
-        'wishlist_movies': wishlist_movies,
+    return render(request, 'movies/movie/list.html', {
+        'page_obj': page_obj,
         'top_directors': top_directors,
         'top_writers': top_writers,
-     })
-
-    return render(request,
-    'movies/movie/list.html',
-    {'section': 'movies',
-    'movies': movies,
-     'watched_movies': watched_movies,
-     'wishlist_movies': wishlist_movies,
-     'top_directors': top_directors,
-     'top_writers': top_writers,
-     })
+    })
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#     watched_movies = request.user.watched_set.all()  # Получаем все просмотренные фильмы
+#     wishlist_movies = request.user.wishlist_set.all()  # Получаем все фильмы в вишлисте
+#
+#     # Получаем топ-10 режиссеров по количеству фильмов
+#     top_directors = Director.objects.annotate(num_movies=Count('movies_director')).order_by('-num_movies')[:10]
+#     # Получаем топ-10 сценаристов по количеству фильмов
+#     top_writers = Writer.objects.annotate(num_movies=Count('movies_writer')).order_by('-num_movies')[:10]
+#
+#     paginator = Paginator(movies, 5)
+#     page = request.GET.get('page')
+#     movies_only = request.GET.get('movies_only')
+#     try:
+#         movies = paginator.page(page)
+#     except PageNotAnInteger:
+#     # Если страница не является целым числом,
+#     # то доставить первую страницу
+#         movies = paginator.page(1)
+#     except EmptyPage:
+#         if movies_only:
+#         # Если AJAX-запрос и страница вне диапазона,
+#         # то вернуть пустую страницу
+#             return HttpResponse('')
+#     # Если страница вне диапазона,
+#     # то вернуть последнюю страницу результатов
+#         movies = paginator.page(paginator.num_pages)
+#     if movies_only:
+#         return render(request,
+#     'movies/movie/list_movies.html',
+#     {'section': 'movies',
+#     'movies': movies,
+#       'watched_movies': watched_movies,
+#         'wishlist_movies': wishlist_movies,
+#         'top_directors': top_directors,
+#         'top_writers': top_writers,
+#      })
+#
+#     return render(request,
+#     'movies/movie/list.html',
+#     {'section': 'movies',
+#     'movies': movies,
+#      'watched_movies': watched_movies,
+#      'wishlist_movies': wishlist_movies,
+#      'top_directors': top_directors,
+#      'top_writers': top_writers,
+#      })
