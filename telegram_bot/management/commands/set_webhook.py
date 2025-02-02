12238e548ec0406c2b0b02cf5f977f50b4aa1273
andreynetrebin <andrei.sanich89@yandex.ru>
2025-02-02 15:12:57 +0300

    fixed env variables

--- telegram_bot/management/commands/set_webhook.py
+++ telegram_bot/management/commands/set_webhook.py
@@ -2,10 +2,11 @@
 from django.core.management.base import BaseCommand
 import requests
 from decouple import config
+
 class Command(BaseCommand):
     help = 'Устанавливает вебхук для Telegram-бота'
 
     def handle(self, *args, **kwargs):
-        webhook_url = f'{config.DOMAIN}/telegram_bot/webhook/'  # Замените на ваш URL
-        response = requests.get(f'https://api.telegram.org/bot{config.TELEGRAM_BOT_TOKEN}/setWebhook?url={webhook_url}')
+        webhook_url = f"{config('SITE_URL')}/telegram_bot/webhook/"  # Замените на ваш URL
+        response = requests.get(f"https://api.telegram.org/bot{config('TELEGRAM_BOT_TOKEN')}/setWebhook?url={webhook_url}")
         print(response.json())
